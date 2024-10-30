3465c33644ebdf0e09d6b0a876dc4ef96008103c
amine-lotfi <amine.swagg99@gmail.com>
2024-10-30 15:46:43 +0100

    Added empty-submission filter

--- index.php
+++ index.php
@@ -43,19 +43,28 @@ if (isset($_POST['submit'])) {
 
 <img src="res/img-logo.png" alt="LOGO" class="img-fluid mb-3">
 <h2>Feedback</h2>
-<p class="lead fs-6">Leave us a feedback to help us improve our services!</p>
+<p class="lead fs-6">Leave us feedback to help us improve our services!</p>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST" class="mt-2 w-75">
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
-        <input type="text" class="form-control" id="name" name="name" placeholder="Enter your name">
+        <input type="text" class="form-control <?php echo !empty($name_error) ? 'is-invalid' : ''; ?>" id="name" name="name" placeholder="Enter your name" value="<?php echo $name; ?>">
+        <div id="validationServerFeedback" class="invalid-feedback">
+            <?php echo $name_error; ?>
+        </div>
     </div>
     <div class="mb-3">
         <label for="email" class="form-label">Email</label>
-        <input type="email" class="form-control" id="email" name="email" placeholder="Enter your email">
+        <input type="email" class="form-control <?php echo !empty($email_error) ? 'is-invalid' : ''; ?>" id="email" name="email" placeholder="Enter your email" value="<?php echo $email; ?>">
+        <div id="validationServerFeedback" class="invalid-feedback">
+            <?php echo $email_error; ?>
+        </div>
     </div>
     <div class="mb-3">
         <label for="feedback" class="form-label">Feedback</label>
-        <textarea class="form-control" id="feedback" name="feedback" placeholder="Enter your feedback"></textarea>
+        <textarea rows="5" class="form-control <?php echo !empty($feedback_error) ? 'is-invalid' : ''; ?>" id="feedback" name="feedback" placeholder="Enter your feedback"><?php echo $feedback; ?></textarea>
+        <div id="validationServerFeedback" class="invalid-feedback">
+            <?php echo $feedback_error; ?>
+        </div>
     </div>
     <div class="d-flex justify-content-center">
         <input type="submit" name="submit" class="btn btn-primary w-25">
